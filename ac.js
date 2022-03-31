2b43f806b6b5fa80dbe71941dc722093eba199ce
bhavishyachandel43 <86842884+bhavishyachandel43@users.noreply.github.com>
2022-03-31 20:06:51 +0530

    Add files via upload

---
+++ ac.js
@@ -0,0 +1,20 @@
+function preload() {
+    img=loadImage('ac.jpg');
+}
+function setup() {
+    canvas=createCanvas(640,420);
+    canvas.center();
+    
+}
+img="";
+function draw() {
+    image(img, 0, 0, 640, 420);
+    fill("#FF0000");
+    text("ac", 45, 75);
+    noFill();
+    stroke("#FF0000");
+   rect(30, 60, 450, 350);
+}
+function back() {
+    window.location="first.html";
+}
